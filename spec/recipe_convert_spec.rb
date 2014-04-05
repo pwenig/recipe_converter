645cf443dd39c2dfe2f673e1f36dc18afefb4822
Paul Wenig <pwenig@gmail.com>
2014-04-04 20:15:00 -0600

    converts ingredients from grams to tablespoons

---
+++ spec/recipe_convert_spec.rb
@@ -0,0 +1,10 @@
+require 'recipe_covert'
+
+describe 'user converts recipe grams to tablespoons and cups' do
+  it "converts ingredients to tablespoons" do
+  conversion = RecipeConverter.new({:sugar => 45, :salt => 60})
+    actual=conversion.to_tablespoons
+    expected = {:sugar => 3, :salt => 4 }
+    expect(actual).to eq(expected)
+  end
+end
